a99b0f5db7d9a59dd43ab5f4e145b668c2e73103
Maemaemaeko <yamaguchi-mae512@g.ecc.u-tokyo.ac.jp>
2023-07-29 21:01:28 +0200

    add results

--- evaluation/Results/get_results_from_rosbag.py
+++ evaluation/Results/get_results_from_rosbag.py
@@ -2,8 +2,7 @@ import rosbag
 import pandas as pd
 
 # Step 1: Specify the ROS bag file path
-rosbag_file = "/home/myamaguchi/catkin_ws/demo_data/vicon_system/2023-07-28-12-17-49.bag"
-
+rosbag_file = "/home/myamaguchi/catkin_ws/demo_data/vicon_system/rgbd_2023-07-28-12-37-18_2023-07-29-20-55-06.bag"
 # Step 2: Extract data from the ROS bag
 timestamps = []
 positions_x = []
@@ -16,15 +15,15 @@ quaternions_z = []
 
 with rosbag.Bag(rosbag_file, "r") as bag:
     for topic, msg, t in bag.read_messages():
-        if topic == "/vicon/realsense_object/realsense_object":  # Replace "your_topic_name" with the actual topic name.
+        if topic == "/orb_slam3/camera_pose":  # Replace "your_topic_name" with the actual topic name.
             timestamps.append(msg.header.stamp.secs*10**9 + msg.header.stamp.nsecs)
-            positions_x.append(msg.transform.translation.x)
-            positions_y.append(msg.transform.translation.y)
-            positions_z.append(msg.transform.translation.z)
-            quaternions_w.append(msg.transform.rotation.w)
-            quaternions_x.append(msg.transform.rotation.x)
-            quaternions_y.append(msg.transform.rotation.y)
-            quaternions_z.append(msg.transform.rotation.z)
+            positions_x.append(msg.pose.position.x)
+            positions_y.append(msg.pose.position.y)
+            positions_z.append(msg.pose.position.z)
+            quaternions_w.append(msg.pose.orientation.w)
+            quaternions_x.append(msg.pose.orientation.x)
+            quaternions_y.append(msg.pose.orientation.y)
+            quaternions_z.append(msg.pose.orientation.z)
 
 # Step 3: Create a DataFrame and save it in the desired format
 data = {
@@ -39,4 +38,4 @@ data = {
 }
 
 df = pd.DataFrame(data)
-df.to_csv("/home/myamaguchi/catkin_ws/src/orb_slam3_ros/evaluation/Ground_truth/vicon_system/2023-07-28-12-17-49.txt", index=False, float_format="%.10f")
+df.to_csv("/home/myamaguchi/catkin_ws/src/orb_slam3_ros/evaluation/Results/vicon_system/rgbd_2023-07-28-12-37-18_2023-07-29-20-55-06.txt", index=False, float_format="%.10f")
